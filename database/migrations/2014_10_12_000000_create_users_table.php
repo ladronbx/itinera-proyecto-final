0eab8a63cb5a91207492b631ce3deaa345aebecf
ladronbx <ladronbravovlc@gmail.com>
2023-12-05 09:28:27 +0100

    [feat] : first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->nullable(false);
+            $table->string('nickname', 100)->unique()->nullable(false);
+            $table->string('email')->unique()->nullable(false);
+            $table->string('password')->nullable(false);
+            $table->string('photo', 255)->default("https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png");
+            $table->string('role', 50)->default("user");
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
