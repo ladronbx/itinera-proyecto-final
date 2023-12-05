82deaa1e8aa9985dcb4ab1995c17abaff5586dae
ladronbx <ladronbravovlc@gmail.com>
2023-12-05 20:27:20 +0100

    [feat]: add necessary fields to the travels migration file

--- database/migrations/2023_12_05_192049_create_travels_table.php
+++ database/migrations/2023_12_05_192049_create_travels_table.php
@@ -4,22 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('travels', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->date('start_date')->nullable(false);
+            $table->date('end_date')->nullable(false);
+            $table->boolean('is_active')->default(true);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('travels');
