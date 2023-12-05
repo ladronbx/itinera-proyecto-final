a863f9d1a514ee5a093132340372bf46fd795ba6
ladronbx <ladronbravovlc@gmail.com>
2023-12-05 21:10:29 +0100

    [feat]: add Activity model with necessary fields

---
+++ app/Models/Activity.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'image_1',
+        'image_2',
+        'duration',
+        'location_id',
+    ];
+}
