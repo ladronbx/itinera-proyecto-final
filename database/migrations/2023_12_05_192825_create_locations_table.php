cc4662e47c6a1343fe92ce74a1bcca876bb28187
ladronbx <ladronbravovlc@gmail.com>
2023-12-05 20:29:45 +0100

    [feat]: add necessary fields to the locations migration file

--- database/migrations/2023_12_05_192825_create_locations_table.php
+++ database/migrations/2023_12_05_192825_create_locations_table.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name', 100)->nullable(false);
+            $table->string('description', 255)->nullable(false);
+            $table->string('image_1', 255)->default("https://cdn.avalos.sv/wp-content/uploads/default-featured-image.png");
+            $table->string('image_2', 255)->default("https://cdn.avalos.sv/wp-content/uploads/default-featured-image.png");
+            $table->string('image_3', 255)->default("https://cdn.avalos.sv/wp-content/uploads/default-featured-image.png");
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('locations');
