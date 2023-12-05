129832f525fd57992c31f4bab0bffe7d4eb5c9cc
ladronbx <ladronbravovlc@gmail.com>
2023-12-05 20:31:45 +0100

    [feat]: add necessary fields to the groups migration file

--- database/migrations/2023_12_05_193026_create_groups_table.php
+++ database/migrations/2023_12_05_193026_create_groups_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users")->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger("travel_id");
+            $table->foreign("travel_id")->references("id")->on("travels")->constrained()->onDelete('cascade');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('groups');
