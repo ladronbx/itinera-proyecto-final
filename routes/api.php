53d00bbcdf1cd2cfda765948a68aef46b58cef03
ladronbx <ladronbravovlc@gmail.com>
2023-12-06 09:39:11 +0100

    [feat]: add user authentication routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// USER
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group([
+    'middleware' => ['auth:sanctum']
+], function () {
+Route::get('/profile', [AuthController::class, 'profile']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::put('/user-update', [UserController::class, 'updateProfile']);
+Route::delete('/user-delete', [UserController::class, 'deleteUser']);
 });
