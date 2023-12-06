cbea86dd56e7a2f0813c4b2b30fd842b8aec9f1f
ladronbx <ladronbravovlc@gmail.com>
2023-12-06 11:17:53 +0100

    [feat]: add seeds to UserSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,9 +8,9 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // $this->call([
-        //     UserSeeder::class,
-        // ]);
+        $this->call([
+            UserSeeder::class,
+        ]);
 
         // \App\Models\User::factory(8)->create();
     }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(30),
+            'email' => 'super@super.com',
+            'password' => Hash::make('Password1!'),
+            'role' => 'super_admin',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => Str::random(30),
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('Password1!'),
+            'role' => 'admin',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => Str::random(30),
+            'email' => Str::random(30).'@user.com',
+            'password' => Hash::make('Password1!')
+        ]);
+
+        DB::table('users')->insert([
+            'name' => Str::random(30),
+            'email' => Str::random(30).'@user.com',
+            'password' => Hash::make('Password1!')
+        ]);
+
+        DB::table('users')->insert([
+            'name' => Str::random(30),
+            'email' => Str::random(30).'@user.com',
+            'password' => Hash::make('Password1!')
+        ]);
+    }
+}
