94b49f00959d40d343ba5ec8e071c7c844492009
ladronbx <ladronbravovlc@gmail.com>
2023-12-06 11:28:06 +0100

    [feat]: add seeds for Travel model

--- database/seeders/TravelSeeder.php
+++ database/seeders/TravelSeeder.php
@@ -4,14 +4,62 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class TravelSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('travels')->insert([
+            'start_date' => now(),
+            'end_date' => now()->addDays(7)
+        ]);
+
+        DB::table('travels')->insert([
+            'start_date' => now()->addDays(7),
+            'end_date' => now()->addDays(12)
+        ]);
+
+        DB::table('travels')->insert([
+            'start_date' => now()->addDays(1),
+            'end_date' => now()->addDays(9)
+        ]);
+
+        DB::table('travels')->insert([
+            'start_date' => now(),
+            'end_date' => now()->addDays(4)
+        ]);
+
+        DB::table('travels')->insert([
+            'start_date' => now()->addDays(3),
+            'end_date' => now()->addDays(6)
+        ]);
+
+        DB::table('travels')->insert([
+            'start_date' => now()->addDays(7),
+            'end_date' => now()->addDays(14)
+        ]);
+
+        DB::table('travels')->insert([
+            'start_date' => now()->addDays(2),
+            'end_date' => now()->addDays(9)
+        ]);
+
+        DB::table('travels')->insert([
+            'start_date' => now()->addDays(1),
+            'end_date' => now()->addDays(8)
+        ]);
+
+        DB::table('travels')->insert([
+            'start_date' => now()->addDays(3),
+            'end_date' => now()->addDays(5)
+        ]);
+
+        DB::table('travels')->insert([
+            'start_date' => now()->addDays(5),
+            'end_date' => now()->addDays(15)
+        ]);
     }
 }
