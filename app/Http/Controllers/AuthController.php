dada2e9656019536c2e9a18d3072ed88ea330f5c
ladronbx <ladronbravovlc@gmail.com>
2023-12-06 10:16:05 +0100

    [feat]: add user registration functionality in AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $validator = $this->validateRegister($request);
+
+            if ($validator->fails()) {
+                return response()->json(
+                    [
+                        "success" => false,
+                        "message" => "Error registering user",
+                        "error" => $validator->errors()
+                    ],
+                    Response::HTTP_BAD_REQUEST
+                );
+            }
+
+            $newUser = User::create([
+                "name" => $request->input('name'),
+                "email" => $request->input('email'),
+                "password" => bcrypt($request->input('password')),
+                "image" => $request->input('image', 'https://media.istockphoto.com/id/1300845620/es/vector/icono-de-usuario-plano-aislado-sobre-fondo-blanco-s%C3%ADmbolo-de-usuario-ilustraci%C3%B3n-vectorial.jpg?s=612x612&w=0&k=20&c=grBa1KTwfoWBOqu1n0ewyRXQnx59bNHtHjvbsFc82gk='),
+            ]);
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "User registered",
+                    "data" => $newUser
+                ],
+                Response::HTTP_CREATED
+            );
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error registering user"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    private function validateRegister(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3|max:100',
+            'email' => 'required|unique:users|email',
+            'password' => 'required|min:6|max:12|regex:/^[a-zA-Z0-9._-]+$/',
+            'image' => 'max:255',
+        ]);
+
+        return $validator;
+    }
+}
