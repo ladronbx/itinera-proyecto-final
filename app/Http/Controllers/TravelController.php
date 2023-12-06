bc0e868dbe382a1e95dc4f9512c6a8d3196591a0
ladronbx <ladronbravovlc@gmail.com>
2023-12-06 11:00:16 +0100

    [feat]: add get all travels functionality in TravelController

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Travel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
 
 class TravelController extends Controller
 {
-    //
+    public function getAllTravels(Request $request)
+    {
+        try {
+            $travels = Travel::query()->get();
+
+            if($travels->isEmpty()){
+                return response()->json(
+                    [
+                        "success" => true,
+                        "message" => "There are not any travel", 
+                    ],
+                    Response::HTTP_OK
+                ); 
+            }
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Travels obtained succesfully",
+                    "data" => $travels
+                ],
+                Response::HTTP_OK
+            );
+
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error obtaining the travels"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
 }
