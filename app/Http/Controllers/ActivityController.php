f888a80fd116fef44666da69db98cc42019b0c96
ladronbx <ladronbravovlc@gmail.com>
2023-12-11 12:22:30 +0100

    [fix]: ensure getActivitiesById returns the required information: 'name', 'location', 'description', 'image_1', 'image_2'

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -10,16 +10,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ActivityController extends Controller
 {
-    public function getAllActivities(Request $request)
+    public function getAllActivities()
     {
         try {
             $activities = Activity::get();
 
-
-
             $data = $activities->map(function ($activity) {
                 $location = Location::find($activity->location_id);
-    
+
                 return [
                     "name" => $activity->name,
                     "location" => $location ? $location->name : null,
@@ -38,51 +36,15 @@ class ActivityController extends Controller
                     ],
                     Response::HTTP_OK
                 );
-            }else{
+            } else {
                 return response()->json(
                     [
                         "success" => false,
                         "message" => "Activities not found"
                     ],
                     Response::HTTP_NOT_FOUND
-                );}
-
-        } catch (\Throwable $th) {
-            Log::error($th->getMessage());
-
-            return response()->json(
-                [
-                    "success" => false,
-                    "message" => "Error obtaining an activity"
-                ],
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
-        }
-    }
-
-    public function getActivityById(Request $request, $id)
-    {
-        try {
-            $activity = Activity::find($id);
-
-            if (!$activity) {
-                return response()->json(
-                    [
-                        "success" => false,
-                        "message" => "Activity not found"
-                    ],
-                    Response::HTTP_NOT_FOUND
                 );
             }
-
-            return response()->json(
-                [
-                    "success" => true,
-                    "message" => "Activity obtained successfully",
-                    "data" => $activity
-                ],
-                Response::HTTP_CREATED
-            );
         } catch (\Throwable $th) {
             Log::error($th->getMessage());
 
@@ -96,7 +58,58 @@ class ActivityController extends Controller
         }
     }
 
-    public function getActivityByLocationId(Request $request, $id)
+    public function getActivityById($id)
+{
+    try {
+        $activity = Activity::find($id);
+
+        if (!$activity) {
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Activity not found"
+                ],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        $location = Location::find($activity->location_id);
+
+        $data = [
+            "name" => $activity->name,
+            "location" => $location ? $location->name : null,
+            "description" => $activity->description,
+            "image_1" => $activity->image_1,
+            "image_2" => $activity->image_2
+        ];
+
+        return response()->json(
+            [
+                "success" => true,
+                "message" => "Activity obtained successfully",
+                "data" => $data
+            ],
+            Response::HTTP_OK
+        );
+
+    } catch (\Throwable $th) {
+        Log::error($th->getMessage());
+
+        return response()->json(
+            [
+                "success" => false,
+                "message" => "Error obtaining an activity"
+            ],
+            Response::HTTP_INTERNAL_SERVER_ERROR
+        );
+    }
+}
+
+
+
+      
+
+    public function getActivityByLocationId($id)
     {
         try {
             $activity = Activity::where('location_id', $id)->get();
@@ -125,7 +138,7 @@ class ActivityController extends Controller
             return response()->json(
                 [
                     "success" => false,
-                    "message" => "Error obtaining a trip"
+                    "message" => "Error obtaining a activity"
                 ],
                 Response::HTTP_INTERNAL_SERVER_ERROR
             );
